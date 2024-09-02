f4f2217c66453e6caa719beb78e7b296171cdae7
nitin <nitinrana01125532553@gmail.com>
2024-09-02 16:39:15 +0530

    added client side pdf parsing

--- server/server.ts
+++ server/server.ts
@@ -5,9 +5,11 @@ import fs from "fs";
 import multer from "multer";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Request, Response } from "express";
+import dotenv from "dotenv";
 
 // make a basic express app
 const app: Express = express();
+dotenv.config();
 
 // allow cross-origin requests
 app.use(cors());
@@ -23,7 +25,7 @@ const Gemini: GoogleGenerativeAI = new GoogleGenerativeAI(
 const imageStorage: multer.StorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
     // file will be stored in public directory
-    cb(null, "public");
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname); // file name will be date + original file name
@@ -32,17 +34,50 @@ const imageStorage: multer.StorageEngine = multer.diskStorage({
 
 const upload = multer({ storage: imageStorage }).single("file");
 
-let filePath;
+let filePath: string = "";
 
 app.post("/upload", (req: Request, res: Response) => {
   upload(req, res, (err) => {
     // handles upload
     if (err) {
       // if error
+      console.log("error: " + err);
       return res.send(500).json(err);
     }
-    filePath = req.file?.path; // record the filePath from request
+    if (req.file?.path) {
+      filePath = req.file.path; // record the filePath from request
+      console.log(filePath);
+    }
   });
+});
+
+const imageToData = (path: string, mimeType: string) => {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+};
+
+app.post("/analyse", async (req, res) => {
+  try {
+    const model = Gemini.getGenerativeModel({
+      model: "gemini-1.5-flash-latest",
+    });
+
+    const question: string = req.body.message; // get the question from request body
+    const analysis = await model.generateContent([
+      // generate the response using question and image's base64 data
+      question,
+      imageToData(filePath, "image/jpeg"),
+    ]);
+    const response = await analysis.response;
+    const output = response.text();
+    res.send(output);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // start the server
